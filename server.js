95ea1c8454b9d3e4349c1e918af5d76187ef7b75
mustafatumertan <mustafatumertan@gmail.com>
2022-08-27 23:10:08 +0300

    Second Commit

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+// Read the ENV file using the DOTENV package
+dotenv.config();
 
 // Run the server
-app.listen(5000, ()=>console.log("Server running in 5000"));
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server running in ${port}`));
